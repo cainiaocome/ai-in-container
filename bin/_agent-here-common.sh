79eb9f4bb325061d7896161dc074deb70934f42f
cainiaocome <cainiaocome@gmail.com>
2026-09-25 09:53:33 -0230

    Store nested Docker data on ephemeral volume

--- bin/_agent-here-common.sh
+++ bin/_agent-here-common.sh
@@ -42,6 +42,7 @@ agent_here_build_docker_args() {
     --name "${AGENT_HERE_CONTAINER_NAME}"
     -v "${AGENT_HERE_HOME_DIR_ON_HOST}:${AGENT_HERE_HOME_DIR_IN_CONTAINER}"
     -v "${PWD}:${AGENT_HERE_WORKDIR}"
+    -v /var/lib/docker
     -w "${AGENT_HERE_WORKDIR}"
   )
 
